a6cb5472ef2f6fda1004606da444814111f97ddd
jaewan876 <jaewcode@gmail.com>
2023-11-06 04:52:30 -0500

    add product card to home page

--- app/Views/index.php
+++ app/Views/index.php
@@ -16,18 +16,30 @@
 </section>
 <section class="home-product py-4">
 	<div class="container">
+		<h3 class="mb-3">Products</h3>
 		<div class="row">
 			<?php if (!empty($products)): ?>
 				<?php foreach ($products as $key => $product): ?>
 					<div class="col-md-3">
-						product
+						
+						<div class="card" style="width: 18rem;">
+							<div class="card-image" style="width: 100%; height: 180px; overflow-y: hidden;">
+								<img src="<?= $product['image'] ?>" class="card-img-top" alt="<?= $product['name'] ?>">
+							</div>
+							<div class="card-body">
+								<h5 class="card-title"><?= $product['name'] ?></h5>
+								<p class="card-text"><?= $product['description'] ?></p>
+								<button class="btn btn-primary">Add to cart</button>
+							</div>
+						</div>
+
 					</div>
 				<?php endforeach ?>
 			<?php endif ?>
 		</div>
 	</div>
 </section>
-<section class="home-service py-4" style="background-color: grey">
+<section class="home-service py-4">
 	<div class="container">
 		<div class="row">
 			<div class="col">
