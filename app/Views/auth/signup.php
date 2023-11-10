c177eaf257f566890ad66fe3bca0a1c13b29c237
jaewan876 <jaewcode@gmail.com>
2023-11-10 11:17:41 -0500

    add proper spacing in signup form

--- app/Views/auth/signup.php
+++ app/Views/auth/signup.php
@@ -5,51 +5,63 @@
 <div class="container">
 
 	<div class="row d-flex align-items-center justify-content-center mt-5">
-
-        
         <div class="col-xl-4 col-lg-6 col-md-6 bg-light">
 
             <!-- SIGNUP FORM -->
             <form method="post" action="<?= base_url('signup') ?>" class="bg-light p-3">
                 <?= csrf_field() ?>
-                <div class="row g-3 mb-3">
+                <div class="row">
                     <div class="col-12">
                         <h4>Signup</h4>
+                        <hr>
                         
                         <?php if(session()->has('error')): ?>
                             <span class="text-danger"><?= session()->get('error') ?></span>
                         <?php endif ?>
+
+                        <input type="hidden" name="redirect_error" value="<?= current_url() ?>">
+                        <input type="hidden" name="redirect_success" value="<?= base_url('login') ?>">
                     </div>
                 </div>
-                <div class="row g-3 mb-3">
+                <div class="row mb-3">
                     <div class="col-md-6">
-                        <label class="form-label">Firstname</label>
-                        <input name="firstname" type="text" class="form-control" placeholder="Firstname" required>
+                        <div class="form-group">
+                            <label class="form-label">Firstname</label>
+                            <input name="firstname" type="text" class="form-control" placeholder="Firstname" required>
+                        </div>
                     </div>
                     <div class="col-6">
-                        <label class="form-label">Lastname</label>
-                        <input name="lastname" type="text" class="form-control" placeholder="Lastname" required>
+                        <div class="form-group">
+                            <label class="form-label">Lastname</label>
+                            <input class="form-control" name="lastname" type="text" placeholder="Lastname" required>
+                        </div>
                     </div>  
                 </div>
-                <div class="row g-3 mb-3">
+                <div class="row mb-3">
                     <div class="col-12">
-                        <label class="form-label" for="">Email</label>
-                        <input name="email" type="email" class="form-control" placeholder="Enter email" value="<?= set_value('email') ?>" required>
+                        <div class="form-group">
+                            <label class="form-label">Email</label>
+                            <input class="form-control" name="email" type="email" placeholder="Enter email" value="<?= set_value('email') ?>" required>
+                        </div>
                     </div> 
                 </div>
-                <div class="row g-3 mb-3">
+                <div class="row mb-3">
                     <div class="col-12">
-                        <label class="form-label">Password</label>
-                        <input name="password" type="password" class="form-control" placeholder="Password" required>
+                        <div class="form-group">
+                            <label class="form-label">Password</label>
+                            <input class="form-control" name="password" type="password" placeholder="Password" required>
+                        </div>
                     </div>                    
                 </div>
-                <div class="row g-3 mb-3">
+                <div class="row mb-3">
                     <div class="col-12">
-                        <label class="form-label">Confirm Password</label>
-                        <input name="password_confirm" type="password" class="form-control" placeholder="Password" required>
+                        <div class="form-group">
+                            <label class="form-label">Confirm Password</label>
+                            <input class="form-control" name="password_confirm" type="password" placeholder="Password" required>
+                        </div>
                     </div>  
                 </div>
-                <div class="row g-3">
+                <div class="row mb-3">
                     <div class="col-12">
                         <button type="submit" class="form-control btn btn-primary">Create Account</button>
                     </div>
@@ -58,9 +70,6 @@
             <!-- SIGNUP FORM -->
 
         </div>
-
-
-
     </div>
 
 </div>
