86ea41388d3a9b4b736ef93f76d23236b8748d61
jaewan876 <jaewcode@gmail.com>
2023-11-10 11:36:13 -0500

    relocate files to pages

---
+++ app/Views/pages/auth/login.php
@@ -0,0 +1,62 @@
+<?= $this->extend('layout/default_template') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container">
+
+	<div class="row align-items-center justify-content-center mt-5">
+        <div class="col-xl-4 col-lg-6 col-md-6 bg-light">
+
+            <!-- LOGIN FORM -->
+            <form method="post" action="<?= base_url('login') ?>" class="login-form bg-light p-3">
+                <?= csrf_field() ?>
+                <div class="row">
+                    <div class="col-12">
+                        <h4>Login</h4>
+                        <hr>
+                        
+                        <?php if(session()->has('error')): ?>
+                            <span class="text-danger"><?= session()->get('error') ?></span>
+                        <?php endif ?>
+                    </div>
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="" class="form-label">
+                                Email
+                            </label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">
+                                    <i class="bi bi-lock"></i>
+                                </span>
+                                <input class="form-control" name="email" type="email" placeholder="Enter email" value="<?= set_value('email') ?>" required>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row g-3">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label for="" class="form-label">
+                                Password
+                            </label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">
+                                    <i class="bi bi-key"></i>
+                                </span>
+                                <input class="form-control" name="password" type="password" placeholder="Enter password" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <button type="submit" class="form-control btn btn-primary">Log In</button>
+                    </div>
+                </div>
+            </form>
+            <!-- LOGIN FORM -->
+
+        </div>
+    </div>
+
+</div>
+
+<?= $this->endSection() ?>
---
+++ app/Views/pages/auth/signup.php
@@ -0,0 +1,77 @@
+<?= $this->extend('layout/default_template') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container">
+
+	<div class="row d-flex align-items-center justify-content-center mt-5">
+        <div class="col-xl-4 col-lg-6 col-md-6 bg-light">
+
+            <!-- SIGNUP FORM -->
+            <form method="post" action="<?= base_url('signup') ?>" class="bg-light p-3">
+                <?= csrf_field() ?>
+                <div class="row">
+                    <div class="col-12">
+                        <h4>Signup</h4>
+                        <hr>
+                        
+                        <?php if(session()->has('error')): ?>
+                            <span class="text-danger"><?= session()->get('error') ?></span>
+                        <?php endif ?>
+
+                        <input type="hidden" name="redirect_error" value="<?= current_url() ?>">
+                        <input type="hidden" name="redirect_success" value="<?= base_url('login') ?>">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label class="form-label">Firstname</label>
+                            <input name="firstname" type="text" class="form-control" placeholder="Firstname" required>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="form-group">
+                            <label class="form-label">Lastname</label>
+                            <input class="form-control" name="lastname" type="text" placeholder="Lastname" required>
+                        </div>
+                    </div>  
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label class="form-label">Email</label>
+                            <input class="form-control" name="email" type="email" placeholder="Enter email" value="<?= set_value('email') ?>" required>
+                        </div>
+                    </div> 
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label class="form-label">Password</label>
+                            <input class="form-control" name="password" type="password" placeholder="Password" required>
+                        </div>
+                    </div>                    
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12">
+                        <div class="form-group">
+                            <label class="form-label">Confirm Password</label>
+                            <input class="form-control" name="password_confirm" type="password" placeholder="Password" required>
+                        </div>
+                    </div>  
+                </div>
+                <div class="row mb-3">
+                    <div class="col-12">
+                        <button type="submit" class="form-control btn btn-primary">Create Account</button>
+                    </div>
+                </div>
+            </form>
+            <!-- SIGNUP FORM -->
+
+        </div>
+    </div>
+
+</div>
+
+<?= $this->endSection() ?>
