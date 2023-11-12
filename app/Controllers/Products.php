99ef1b6c10435d02a7ba601b64e672d59d4b8c4f
jaewan876 <jaewcode@gmail.com>
2023-11-12 11:17:42 -0500

    add model to product controller

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -4,12 +4,25 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 
+use App\Models\ProductModel;
+use App\Models\CategoryModel;
+
 class Products extends BaseController
 {
+    protected $productModel;
+    protected $categoryModel;
+
+    public function __construct()
+    {
+        $this->productModel = new ProductModel();
+        $this->categoryModel = new CategoryModel();
+    }
+
     public function index()
     {
         $data = [
-            'title' => 'Products'
+            'title' => 'Products',
+            'products' => $this->productModel->find(),
         ];
 
         return view('pages/products/list', $data);
