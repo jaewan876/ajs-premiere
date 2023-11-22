c157ff58a00964b9693d4bc2dac647e99b6fce03
jaewan876 <jaewcode@gmail.com>
2023-11-22 09:27:51 -0500

    update landing page

--- app/Views/index.php
+++ app/Views/index.php
@@ -13,14 +13,17 @@
 				<p class="text-center text-md-start">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis accusantium quasi corporis provident ad hic quae, reiciendis consequatur distinctio labore odit, esse alias voluptates non.
 				</p>
-				<div class="text-center">
-					<a class="btn btn-dark text-center" href="<?= base_url('products') ?>">
-						<i class="bi bi-cart"></i> Shop Now
+				<div class="text-center text-md-start">
+					<a class="btn btn-success btn-lg fw-bold" href="<?= base_url('products') ?>">
+						Shop Now <i class="bi bi-cart"></i> 
+					</a>
+					<a class="btn btn-outline-dark btn-lg fw-bold" href="<?= base_url('contact-us') ?>">
+						Contact Us <i class="bi bi-arrow-right"></i>
 					</a>
 				</div>
 			</div>
 			<div class="col-md-6">
-				<img src="<?= base_url('assets/images/static/digital-print-2.png') ?>" alt="" class="hero-image img-fluid" width="100%" loading="lazy">
+				<img src="<?= base_url('assets/images/static/digital-print-2.png') ?>" alt="Hero" class="hero-image" loading="lazy" width="75%">
 			</div>
 		</div>
 	</div>
@@ -42,29 +45,29 @@
 	</div>
 </section>
 
-<section class="home-product py-4 bg-light">
+<section class="home-product py-5 bg-light">
 	<div class="container">
 		<h3 class="mb-4">Feature Products</h3>
 		<div class="row">
 			<?php if (!empty($products)): ?>
 				<?php foreach ($products as $key => $product): ?>
 					<div class="col-sm-6 col-md-6 col-lg-3 mb-3">
-						
-						<div class="card">
-							<div class="card-image" style="">
-								<img src="<?= $product['image'] ?>" class="card-img-top" alt="<?= $product['name'] ?>" loading="lazy">
-							</div>
-							<div class="card-body">
-								<h5 class="card-title"><?= $product['name'] ?></h5>
-								<div class="d-flex align-items-center justify-content-between">
-									<span><strong>$<?= $product['price'] ?></strong></span>
-									<button class="btn btn-primary btn-sm" id="add_to_cart" data-id="<?= $product['product_id'] ?>">
-										Add to cart
-									</button>
+						<a class="text-decoration-none" href="<?= base_url('products/'.$product['product_id']) ?>">
+							<div class="card product-card">
+								<div class="card-image" style="">
+									<img src="<?= $product['image'] ?>" class="card-img-top" alt="<?= $product['name'] ?>" loading="lazy">
+								</div>
+								<div class="card-body">
+									<h5 class="card-title"><?= $product['name'] ?></h5>
+									<div class="d-flex align-items-center justify-content-between">
+										<span><strong>$<?= $product['price'] ?></strong></span>
+										<button class="btn btn-warning btn-sm" id="add_to_cart" data-id="<?= $product['product_id'] ?>">
+											Add to cart
+										</button>
+									</div>
 								</div>
 							</div>
-						</div>
-
+						</a>
 					</div>
 				<?php endforeach ?>
 			<?php endif ?>
