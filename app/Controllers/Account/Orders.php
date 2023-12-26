3bb85f0d6e76cd2b07f5fc1399fc032b13a8133a
jaewan876 <jaewcode@gmail.com>
2023-12-26 00:57:06 -0500

    update orders controller

--- app/Controllers/Account/Orders.php
+++ app/Controllers/Account/Orders.php
@@ -4,10 +4,64 @@ namespace App\Controllers\Account;
 
 use App\Controllers\BaseController;
 
+use App\Models\OrderModel;
+use App\Models\OrderItemModel;
+use App\Models\OrderStatusModel;
+use App\Models\PaymentModel;
+
 class Orders extends BaseController
 {
+    protected $orderModel;
+    protected $orderItemModel;
+    protected $statusModel;
+    protected $paymentModel;
+
+    public function __construct()
+    {
+        $this->orderModel = new OrderModel();
+        $this->orderItemModel = new OrderItemModel();
+        $this->statusModel = new OrderStatusModel();
+        $this->paymentModel = new PaymentModel();
+    }
+
     public function index()
     {
-        //
+        $orders = $items = [];
+        $customer_id = session()->get('customer_id');
+
+        $orders = $this->orderModel->where(['customer_id' => $customer_id])->find();
+
+        if($orders){
+            $items = $this->orderItemModel->find_products(['order_id' => $orders[0]['order_id']]);
+        }
+        
+
+        $data = [
+            'title' => 'Orders',
+            'orders' => $orders,
+            'items' => $items,
+        ];
+
+        return view('customer/orders/list', $data);
+    }
+
+    public function show($order_id = null)
+    {
+        $orders = $items = [];
+
+        $orders = $this->orderModel->where(['order_id' => $order_id])->find();
+
+        if($orders){
+            $items = $this->orderItemModel->find_products(['order_id' => $order_id]);
+        }
+        
+
+        $data = [
+            'title' => 'Orders',
+            'order' => $orders,
+            'items' => $items,
+        ];
+
+        return view('customer/orders/show', $data);
     }
 }
