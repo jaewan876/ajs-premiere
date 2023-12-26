38c65addce91c3b58029ac7a80ebe7b38725595a
jaewan876 <jaewcode@gmail.com>
2023-12-26 01:00:02 -0500

    update shopping cart page

--- app/Views/pages/shop/cart.php
+++ app/Views/pages/shop/cart.php
@@ -12,55 +12,118 @@
 	<section class="row cart">
 		<div class="col pb-4">
 			<table class="table">
-				<th class="th-lg">Product</th>
-				<th>Price</th>
-				<th>Quantity</th>
-				<th>Total</th>
+				<th colspan="3" class="th-lg">Item</th>
 				<th></th>
+				<th colspan="1">Price</th>
 				<tbody>
 					<?php foreach ($items as $key => $value): ?>
 					<tr>
-						<td><?= $value['product_id'] ?></td>
-						<td><?= number_to_currency($value['item_price'], 'USD', 'en_US', 2) ?></td>
-						<td scope="1">
-							<div class="input-group">
-								<button class="btn">
-									<i class="bi bi-plus"></i>
-								</button>
-								<input class="text-center" type="number" size="5" value="<?= $value['item_qty'] ?>" min="1" max="10" readonly>
-								<button class="btn">
-									<i class="bi bi-dash"></i>
-								</button>
+						<td colspan="3">
+							<p>
+								<?= $value['name'] ?>
+							</p>
+
+							<div class="d-inline-block">
+								<form method="post" action="<?= base_url('cart/update/'.$value['item_id']) ?>">
+									<input type="hidden" name="redirect_success" value="<?= current_url() ?>">
+									<input type="hidden" name="redirect_error" value="<?= current_url() ?>">
+									<input type="hidden" name="item_price" value="<?= $value['price'] ?>">
+									<select name="item_qty" id="cart_item_qty" onchange='this.form.submit()'>
+										<?php for ($i = 1; $i <= 20; $i++): ?>
+											<option value="<?= $i ?>" <?= ($i == $value['item_qty'] ? 'selected':'') ?>><?= $i ?></option>
+										<?php endfor ?>
+									</select>
+								</form>
 							</div>
+
+							<div class="" hidden>
+								<form method="post" action="<?= base_url('cart/update/'.$value['item_id']) ?>">
+									<input type="hidden" name="redirect_success" value="<?= current_url() ?>">
+									<input type="hidden" name="redirect_error" value="<?= current_url() ?>">
+									<div class="input-group input-group-sm border" id="cart_item_qty" data-id="<?= $value['item_id'] ?>">
+										<button class="btn btn-light btn-outline-secondary btn-minus border" type="button">
+											<i class="bi bi-plus"></i>
+										</button>
+										<input class="border text-center" name="item_qty" type="number" value="<?= $value['item_qty'] ?>" min="1" max="10" readonly>
+										<button class="btn btn-plus btn-outline-secondary border" type="button">
+											<i class="bi bi-dash"></i>
+										</button>
+									</div>
+								</form>
+							</div>
+							<div class="d-inline-block px-1">
+								<form method="post" action="<?= base_url('cart/remove/'.$value['item_id']) ?>">
+									<input type="hidden" name="redirect_success" value="<?= current_url() ?>">
+									<input type="hidden" name="redirect_error" value="<?= current_url() ?>">
+									<button class="btn text-danger" title="Delete">
+										<i class="bi bi-x-lg"></i> Delete
+									</button>
+								</form>
+							</div> 
+							
+							
 						</td>
-						<td><?= number_to_currency($value['item_price'] * $value['item_qty'], 'USD', 'en_US', 2) ?></td>
-						<td>
-							<form method="post" action="<?= base_url('cart/remove/'.$value['item_id']) ?>">
-								<input type="hidden" name="redirect_success" value="<?= current_url() ?>">
-								<input type="hidden" name="redirect_error" value="<?= current_url() ?>">
-								<button class="btn text-danger" title="Delete">
-									<i class="bi bi-x-lg"></i>
-								</button>
-							</form>
-						</td>
+						<td></td>
+						<td colspan="1"><?= number_to_currency($value['item_price'] ?? 0, 'USD', 'en_US', 2) ?></td>
 					</tr>
 					<?php endforeach ?>
+					
+					<?php if ($subtotal): ?>
+					<tr>
+						<td colspan="3"></td>
+						<td colspan="1">
+							Subtotal (<?= $quantity ?> <?= $quantity > 1 ? 'items':'item' ?>):
+						</td>
+						<td colspan="1">
+							<?= number_to_currency($subtotal ?? 0, 'USD', 'en_US', 2) ?>
+						</td>
+					</tr>
+					<?php endif ?>
+					
 				</tbody>
 			</table>
 		</div>
 		<div class="col-3">
-			<h5>Summary</h5>
+			<div>
+				<table class="table table-borderless">
+					<tbody>
+						<tr>
+							<td>Subtotal</td>
+							<td><?= number_to_currency($subtotal ?? 0, 'USD', 'en_US', 2) ?></td>
+						</tr>
+						<tr>
+							<td>Tax</td>
+							<td><?= number_to_currency(0, 'USD', 'en_US', 2) ?></td>
+						</tr>
+						<tr>
+							<td>Discount</td>
+							<td><?= number_to_currency(0, 'USD', 'en_US', 2) ?></td>
+						</tr>
+						<tr>
+							<td>
+								<b>Total</b>
+							</td>
+							<td>
+								<b><?= number_to_currency($total ?? 0, 'USD', 'en_US', 2) ?></b>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 			<div class="mb-3">
 				<a class="btn btn-light" href="<?= base_url('products') ?>">
 					<i class="bi bi-arrow-left"></i> Continue Shopping
 				</a>
 			</div>
 
+			<?php if ($total): ?>
 			<div class="d-flex align-items-center">
 				<a class="btn btn-success" href="<?= base_url('checkout') ?>">
 					Checkout <i class="bi bi-arrow-right"></i>
 				</a>
 			</div>
+			<?php endif ?>
+			
 		</div>
 	</section>
 </div>
