5a214cedae9b359ba8158bc24efb8d113a49472c
jaewan876 <jaewcode@gmail.com>
2023-12-27 00:11:57 -0500

    update admin dashboard controller

--- app/Controllers/Admin/Dashboard.php
+++ app/Controllers/Admin/Dashboard.php
@@ -4,12 +4,41 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 
+use App\Models\OrderModel;
+use App\Models\ProductModel;
+use App\Models\CustomerModel;
+
 class Dashboard extends BaseController
 {
+    protected $orderModel;
+    protected $productModel;
+    protected $customerModel;
+    // protected $paymentModel;
+
+    public function __construct()
+    {
+        $this->orderModel = new OrderModel();
+        $this->productModel = new ProductModel();
+        $this->customerModel = new CustomerModel();
+        // $this->paymentModel = new PaymentModel();
+    }
+
     public function index()
     {
+        $orders = $this->orderModel->find();
+        $products = $this->productModel->find();
+        $customers = $this->customerModel->find();
+
+        $stats = [
+            'earnings' => 0,
+            'orders' => count($orders) ?? 0,
+            'products' => count($products) ?? 0,
+            'customers' => count($customers) ?? 0,
+        ];
+
         $data = [
-            'title' => 'Dashboard'
+            'title' => 'Dashboard',
+            'stats' => $stats,
         ];
 
         return view('admin/dashboard', $data);
