4cff131ad33f524b144330d2c876f8de59c9f161
jaewan876 <jaewcode@gmail.com>
2023-12-26 23:54:30 -0500

    update admin's order details page

--- app/Views/admin/orders/show.php
+++ app/Views/admin/orders/show.php
@@ -2,7 +2,7 @@
 
 <?= $this->section('content') ?>
 
-<div class="container">
+<div class="container mb-3">
     <div class="d-flex justify-content-between py-4">
         <div class="d-flex align-items-center">
             <!--  -->
@@ -19,19 +19,80 @@
         </div>
     </div>
 
-	<section class="pb-5">
+	<section class="pb-4">
 		<h1>
 			<?= $title ?>
 		</h1>
 	</section>
 
+	<section class="row mb-5">
+
+		<div class="col-md-6">
+			<table class="table">
+				<tbody>
+					<tr>
+						<td>Order Time</td>
+						<td><?= $orders['order_created_at'] ?></td>
+					</tr>
+					<tr>
+						<td>Order Status</td>
+						<td>
+							<span class="badge bg-info text-dark">
+								<?= ucfirst(str_replace("_", " ", $orders['order_status'])) ?>
+							</span>
+						</td>
+					</tr>
+					<tr>
+						<td>Payment Status</td>
+						<td>
+							<?= $orders['order_is_paid'] == 0 ? 'Pending':'Completed' ?>
+						</td>
+					</tr>
+					<tr>
+						<td>Payment Method</td>
+						<td><?= ucfirst($payment[0]['payment_method']) ?></td>
+					</tr>					
+				</tbody>
+			</table>
+
+			<form method="post" action="<?= base_url('admin/orders/status/'.$orders['order_id']) ?>">
+				<input type="hidden" name="redirect" value="<?= current_url() ?>">
+				<input type="hidden" name="redirect_success" value="<?= current_url() ?>">
+				<div class="form-row">
+					<div class="col-md-4">
+						<div class="form-group">
+							<label class="form-label" for="">Update Status</label>
+							<div class="input-group">
+								<span class="input-group-text">Status</span>
+								<select class="form-select" name="status" onchange='this.form.submit()' required>
+									<option value="">Select Status</option>
+									<option value="pending">Pending</option>
+									<option value="shipped">Shipped</option>
+									<option value="in_transit">In transit</option>
+									<option value="completed">Completed</option>
+									<option value="delivered">Delivered</option>
+									<option value="on_hold">On hold</option>
+									<option value="canceled">Canceled</option>
+									<option value="declined">Declined</option>
+								</select>
+							</div>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+		
+	</section>
+
 	<section class="row">
 		<div class="col">
+			<h5>Item Details</h5>
+
 			<table class="table">
 				<th colspan="3">Item</th>
 				<th>Qty</th>
 				<th colspan="1">Price</th>
-				<th></th>
+				<th>Total Price</th>
 				<tbody>
 					<?php foreach ($items as $key => $value): ?>
 					<tr>
