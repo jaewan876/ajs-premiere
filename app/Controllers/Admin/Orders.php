ce34e4331c46b84b041129f1a519145db1269373
jaewan876 <jaewcode@gmail.com>
2023-12-26 19:17:51 -0500

    update admin's orders controller

--- app/Controllers/Admin/Orders.php
+++ app/Controllers/Admin/Orders.php
@@ -4,15 +4,53 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 
+use App\Models\OrderModel;
+use App\Models\OrderItemModel;
+use App\Models\OrderStatusModel;
+use App\Models\PaymentModel;
+
 class Orders extends BaseController
 {
+    protected $orderModel;
+    protected $orderItemModel;
+    protected $statusModel;
+    protected $paymentModel;
+
+    public function __construct()
+    {
+        $this->orderModel = new OrderModel();
+        $this->orderItemModel = new OrderItemModel();
+        $this->statusModel = new OrderStatusModel();
+        $this->paymentModel = new PaymentModel();
+    }
+
     public function index()
     {
+        $orders = $this->orderModel->find();
+
         $data = [
-            'title' => 'Orders',
-            'orders' => [],
+            'title' => 'All Orders',
+            'orders' => $orders,
         ];
 
         return view('admin/orders/list', $data);
+    }
+
+    public function show($order_id = null)
+    {
+        if ($order_id == null) {
+            // show 404 page
+        }
+
+        $orders = $this->orderModel->find($order_id);
+        $items = $this->orderItemModel->find_products(['order_id' => $order_id]);
+
+        $data = [
+            'title' => 'Order #'.$order_id,
+            'orders' => $orders,
+            'items' => $items,
+        ];
+
+        return view('admin/orders/show', $data);
     }
 }
