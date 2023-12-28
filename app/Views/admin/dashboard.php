f1b895a3ec730759a16173035db5ce1b21188996
jaewan876 <jaewcode@gmail.com>
2023-12-28 13:59:25 -0500

    update admin/dashboard page

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -19,7 +19,7 @@
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Earnings (Monthly)</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= $stats['earnings'] ?></div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800"><?= number_to_currency($stats['earnings'], 'USD', 'en_US', 2) ?></div>
                         </div>
                         <div class="col-auto">
                             <i class="fas fa-calendar fa-2x text-gray-300"></i>
