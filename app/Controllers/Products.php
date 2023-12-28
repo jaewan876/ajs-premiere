fc7cb3e59eb0f258531484da745e78dc8d2abfe2
jaewan876 <jaewcode@gmail.com>
2023-12-28 11:34:56 -0500

    update product controller

--- app/Controllers/Products.php
+++ app/Controllers/Products.php
@@ -7,15 +7,22 @@ use App\Controllers\BaseController;
 use App\Models\ProductModel;
 use App\Models\CategoryModel;
 
+use App\Models\CartModel;
+use App\Models\CartItemModel;
+
 class Products extends BaseController
 {
     protected $productModel;
     protected $categoryModel;
+    protected $cartModel;
+    protected $cartItemModel;
 
     public function __construct()
     {
         $this->productModel = new ProductModel();
         $this->categoryModel = new CategoryModel();
+        $this->cartModel = new CartModel();
+        $this->cartItemModel = new CartItemModel();
     }
 
     public function index()
@@ -30,12 +37,21 @@ class Products extends BaseController
 
     public function show($id = null, $slug = null)
     {
+        $cart_items = [];
+
         $product = $this->productModel->find($id);
+
+        $cart = $this->cartModel->where(['customer_id' => session()->get('customer_id')])->find();
+
+        if ($cart) {
+            $cart_items = $this->cartItemModel->where(['cart_id' => $cart[0]['cart_id'], 'product_id' => $id])->find();
+        }
 
         $data = [
             'title' => $product['name'] ?? 'Not Available',
             'product' => $product,
             'customer' => [],
+            'cart_items' => $cart_items,
         ];
 
         return view('pages/products/view', $data);
